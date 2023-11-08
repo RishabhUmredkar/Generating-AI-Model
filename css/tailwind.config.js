92ca4615890e00411e24f7beadcbab7d70c5f4b0
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-11-08 14:27:41 +0530

    update

---
+++ css/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+
+module.exports = {
+    theme: {
+      extend: {
+        backgroundColor: {
+          'custom-green': '#0b7200',
+        },
+        textColor: {
+          'custom-green': '#0b7200',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
